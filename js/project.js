104d2d12cca15446d9df780a35b2fe79fc3bcc31
Krystsina <kristinakeiko@yandex.com>
2019-02-20 15:53:05 +0300

    Второй коммит

--- js/project.js
+++ js/project.js
@@ -4,15 +4,21 @@ let time = prompt('Введите дату в формате YYYY-MM-DD');
 let appData = {
 	 budget: money,
 	 timeData: time,
-	 expenses: ,
+	 expenses: {} ,
 	 optionalExpenses: {},
 	 income: [],
 	 savings: false,
  }
 
-let    = prompt('Введите обязательную статью расходов в этом месяце', '');
-let    = prompt('Во сколько обойдется', '');
+let   a1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let   a2 = prompt('Во сколько обойдется', '');
+let   a3 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let   a4 = prompt('Во сколько обойдется', '');
 
-let budgetOneDay = alert(appData.budget / 30);
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
 
  
