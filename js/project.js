54beccf41a603d3979a48f93592b43cac73927ea
Krystsina <kristinakeiko@yandex.com>
2019-02-20 15:36:27 +0300

    Второй комит

---
+++ js/project.js
@@ -0,0 +1,18 @@
+" use strict ";
+let money = prompt('Ваш бюджет в месяц', "");
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let appData = {
+	 budget: money,
+	 timeData: time,
+	 expenses: ,
+	 optionalExpenses: {},
+	 income: [],
+	 savings: false,
+ }
+
+let    = prompt('Введите обязательную статью расходов в этом месяце', '');
+let    = prompt('Во сколько обойдется', '');
+
+let budgetOneDay = alert(appData.budget / 30);
+
+ 
